f21440d7ef80ad1dd885719be5e3d34eac6ded04
Matt Rosensweig <matthew.rosensweig@gmail.com>
2020-12-29 19:20:07 -0500

    Drills for created Array class

---
+++ array.js
@@ -0,0 +1,98 @@
+const Memory = require('./memory');
+
+let memory = new Memory();
+
+class Array {
+  constructor() {
+    this.length = 0;
+    this._capacity = 0;
+    this.ptr = memory.allocate(this.length);
+  }
+
+  push(value) {
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);
+    }
+    memory.set(this.ptr + this.length, value);
+    this.length++;
+  }
+
+  _resize(size) {
+    const oldPtr = this.ptr;
+    this.ptr = memory.allocate(size);
+    if (this.ptr === null) {
+      throw new Error('Out of memory');
+    }
+    memory.copy(this.ptr, oldPtr, this.length);
+    memory.free(oldPtr);
+    this._capacity = size;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
+    }
+    return memory.get(this.ptr + index);
+  }
+
+  pop() {
+    if (this.length == 0) {
+      throw new Error('Index error');
+    }
+    const value = memory.get(this.ptr + this.length - 1);
+    this.length--;
+    return value;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
+    }
+
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);
+    }
+
+    memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+    memory.set(this.ptr + index, value);
+    this.length++;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
+    }
+    memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
+    this.length--;
+  }
+}
+
+function main() {
+  Array.SIZE_RATIO = 3;
+
+  // Create an instance of the Array class
+  let arr = new Array();
+
+  // Add an item to the array
+  arr.push(3);
+  // At this point, arr.length = 1, arr._capacity = 3, arr.ptr = 0
+  arr.push(5);
+  arr.push(15);
+  arr.push(19);
+  arr.push(45);
+  arr.push(10);
+  // At this point, arr.length = 6, arr._capacity = 12, arr.ptr = 3
+  // There are currently 12 bytes of memory allocated to the array at addresses 3-14
+  // 6 of those bytes are being used by the array's entries.
+
+  arr.pop();
+  arr.pop();
+  arr.pop();
+  // At this point, arr.length = 3, arr._capacity = 12, arr.ptr = 3
+  // There are still 12 bytes of memory allocated, but now only 3 are being used
+  // The `pop()` function removes the data from memory but it is still allocated to the array
+
+  console.log(arr);
+}
+
+main();
